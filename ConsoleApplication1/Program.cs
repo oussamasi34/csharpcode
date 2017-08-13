9b95f947cccfa6c82fcdd563532a8977ff20a076
fujitsu <fujitsu@OUSSAMASI>
2017-08-13 10:14:46 +0100

    add a cooment

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-           
+           //how are you guys
             Console.WriteLine("hello world");
             Console.ReadKey();
         }
